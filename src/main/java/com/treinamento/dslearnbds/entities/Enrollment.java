a8028129d8341f19107321f401adc95fdbde74a6
Edson Ney de Farias <edson.ney@gmail.com>
2026-08-07 18:24:25 -0300

    Lesson seed, fix: lessonsDone

--- src/main/java/com/treinamento/dslearnbds/entities/Enrollment.java
+++ src/main/java/com/treinamento/dslearnbds/entities/Enrollment.java
@@ -1,13 +1,16 @@
 package com.treinamento.dslearnbds.entities;
 
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.treinamento.dslearnbds.entities.pk.EnrollmentPK;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -25,6 +28,9 @@ public class Enrollment {
 	private boolean available;
 	private boolean onlyUpdate;
 	
+	@ManyToMany(mappedBy = "enrollmentsDone")
+	private Set<Lesson> lessonsDone = new HashSet<>();
+
 	public Enrollment() {
 	}
 
