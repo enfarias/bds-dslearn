f29adebce68c69f05e6eb341b4c85c81772aff99
Edson Ney de Farias <edson.ney@gmail.com>
2026-08-08 19:02:42 -0300

    Fix: missing associations

--- src/main/java/com/treinamento/dslearnbds/entities/Offer.java
+++ src/main/java/com/treinamento/dslearnbds/entities/Offer.java
@@ -36,7 +36,10 @@ public class Offer {
 	
 	@OneToMany(mappedBy = "offer")
 	private List<Resource> resources = new ArrayList<>();
-
+	
+	@OneToMany(mappedBy = "offer")
+	private List<Topic> topics = new ArrayList<>();	
+	
 	public Offer() {
 	}
 
@@ -91,6 +94,10 @@ public class Offer {
 	
 	public List<Resource> getResources() {
 		return resources;
+	}
+
+	public List<Topic> getTopics() {
+		return topics;
 	}
 
 	@Override
